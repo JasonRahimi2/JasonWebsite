28af07ca5a8cdb527f48382668f4647be54ce48b
Jason Rahimi <jasonrahimi@Jasons-MacBook-Pro.local>
2025-02-20 17:14:07 -0500

    Updates

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
     
-    return  <div className='headerDiv'>
+    return  <div className='headerContainer'>
             <h1 id='header'>Jason Rahimi</h1>
             <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'></link>
             <nav>
