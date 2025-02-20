ccf7e88421bb088e501a0e40f65415a7c6001ffb
Jason Rahimi <jasonrahimi@Jasons-MacBook-Pro.local>
2025-02-20 17:27:00 -0500

    Updates

--- src/About.tsx
+++ src/About.tsx
@@ -1,11 +1,9 @@
 function About() {
     return <div className='aboutContainer'>
-        <div className='about'> 
         <h2>About Me</h2>  
         <p>Hi, I'm Jason Rahimi, a versatile professional passionate about both data science and software engineering. I thrive on turning complex data into actionable insights and developing innovative solutions.
            I thrive on turning complex data into actionable insights and developing innovative solutions. My expertise spans data analysis tools such as Python, R, SQL, and Tableau, as well as full-stack technologies
             like HTML, CSS, JavaScript, and Node.js. I'm dedicated to leveraging data and technology to drive innovation and efficiency. Feel free to explore my portfolio, and if you're interested in data-driven solutions or full-stack development, don't hesitate to reach out using the contact form below.</p>
-         </div>
          </div>
 }
 
